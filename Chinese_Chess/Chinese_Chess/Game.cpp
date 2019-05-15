942247ba1b16661bceab4ceb99aeb56de5dc071a
rosa <u10050070@gmail.com>
2019-05-15 08:17:25 +0800

    更新排版

--- Chinese_Chess/Chinese_Chess/Game.cpp
+++ Chinese_Chess/Chinese_Chess/Game.cpp
@@ -101,7 +101,7 @@ void Game::drawCheckerboard(int i, int mode) {//mode:0->一般, 1->目錄, 2, 3-
 		drawStatus();
 	}
 	else if (i == 7) {
-		cout << setw(28) << "←　→　方向鍵控制光標";
+		cout << setw(26) << "←　→　方向鍵控制光標";
 	}
 	if (mode == 0 && i != 9) {
 		if (i == 4 || i == 5) {
@@ -147,14 +147,14 @@ void Game::drawFooter()
 void Game::drawStatus()
 {
 	if (player == 1) {
-		cout << setw(20) << "現在輪到";
+		cout << setw(16) << "現在輪到";
 		SetConsoleTextAttribute(handleSTDOutput, FOREGROUND_RED);
 		cout << "紅方";
 		SetConsoleTextAttribute(handleSTDOutput, originalColor);
 		cout << "下棋";
 	}
 	else if (player == 0) {
-		cout << setw(20) << "現在輪到";
+		cout << setw(16) << "現在輪到";
 		SetConsoleTextAttribute(handleSTDOutput, FOREGROUND_INTENSITY);
 		cout << "黑方";
 		SetConsoleTextAttribute(handleSTDOutput, originalColor);
